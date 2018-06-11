4705c8d4f88229caf2f07198e3986b055bcdcd6e
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-06-11 11:08:43 +0200

    add icons

--- src/components/Course/index.js
+++ src/components/Course/index.js
@@ -4,23 +4,57 @@ import { bindActionCreators } from "redux";
 import PropTypes from 'prop-types'
 
 export default class Course extends Component {
+
+    state = {
+        editCourseView: false
+    }
+
+    handleEditCourse = () => {
+        this.setState({  editCourseView: true})
+    }
+
+    handleCancelEdit = () => {
+        this.setState({ editCourseView: false })
+    }
+    
     render() {
     const { course } = this.props
-        return (
+
+    if (!this.state.editCourseView) {
+        return(
             <tr className = 'dataRow' key = {course.id}>
                 <td>{course.name}</td>
                 <td>{course.lecturer}</td>
                 <td className = 'actionCell'>
                     <i class="fas fa-edit" 
                     title ='Edit course'
-                    onClick = {e => this.handleEditCourse(course)}
+                    onClick = {e => this.handleEditCourse()}
                     ></i>
                     <i class="fas fa-trash" 
                     title ='Delete course'
                     onClick = {e => this.handleRemoveDialogOpen(course)}>
                     </i>
                 </td>
+            </tr> 
+        )
+    }else{
+        return(
+            <tr className = 'dataRow' key = {course.id}>
+                <td>{course.name}</td>
+                <td>{course.lecturer}</td>
+                <td className = 'actionCell'>
+                
+                    <i class="fas fa-check" 
+                    title ='Edit course'
+                    onClick = {e => this.handleEditCourse(course)}
+                    ></i>
+                    <i class="fas fa-times" 
+                    title ='Delete course'
+                    onClick = {e => this.handleCancelEdit()}>
+                    </i>
+                </td>
             </tr>
         )
+    }
   }
 }
